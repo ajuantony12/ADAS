360f2f89e5f502f16e50c127d86bef31506086d6
Hannes Baehr <hbaehr@studenten.hs-bremerhaven.de>
2019-01-07 13:57:07 +0100

    update motor control

--- 06_source/templates/VoltageRegulator_Enc/VoltageRegulator_Enc.ino
+++ 06_source/templates/VoltageRegulator_Enc/VoltageRegulator_Enc.ino
@@ -37,34 +37,24 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(PIN_ENC), EncISR, RISING);
   digitalWrite(PIN_ENABLE, LOW);
   digitalWrite(PIN_DIRECTION, HIGH);
-  t.every(250, readenc, 0);
+  t.every(500, readenc, 0);
   setupPWM16();
 }
 
 void loop()
 {
   if (control) {
-    setpoint = 1023;
-    feedback = enc_t;
+ 
+    setpoint = 200;
+    feedback = counted_peaks*4;
     output = myPID.step(setpoint, feedback);
 
     if (output >= 1023) {
       output = 1023;
     }
-
-    Serial.print("rpm = ");
-    Serial.print(1.0 / (70 * enc_t) * 1e6); // in 1/min
-    Serial.print(" enc_t = ");
-    Serial.println(enc_t); // time between two teeth in microseconds
-
-    // Serial.print("runtime: ");
-    // Serial.print(after - before);
-    Serial.print(" sp: ");
-    Serial.print(setpoint);
-    Serial.print(" fb: ");
-    Serial.print(feedback);
-    Serial.print(" out: ");
-    Serial.println(output);
+    Serial.print(output);
+    Serial.print(";");
+    Serial.println(counted_peaks);
     if (digitalRead(PIN_ENABLE) == HIGH) {
       digitalWrite(LED1, LOW);
       digitalWrite(LED2, LOW);
@@ -73,6 +63,7 @@ void loop()
     }
     control=false;
   }
+  t.update();
 }
 
 
@@ -80,7 +71,6 @@ void writeLED(uint16_t n)
 {
   analogWrite16(LED1, 1023 - n);
   analogWrite16(LED2, 1023 - n);
-  //delayMicroseconds(WAIT);
 }
 
 
@@ -90,9 +80,7 @@ void EncISR(void)
 }
 
 void readenc(void* context) {
-
   counted_peaks = peaks;
   control = true;
   peaks = 0;
-  Serial.print("Peaks übergeben");
 }
