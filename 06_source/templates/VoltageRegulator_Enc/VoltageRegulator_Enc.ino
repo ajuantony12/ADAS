66197ac76cf1c63626c73af36c0c23111b3336c0
Hannes Baehr <hbaehr@studenten.hs-bremerhaven.de>
2019-01-07 14:17:45 +0100

    Speed control with PI Control for both motors

--- 06_source/templates/VoltageRegulator_Enc/VoltageRegulator_Enc.ino
+++ 06_source/templates/VoltageRegulator_Enc/VoltageRegulator_Enc.ino
@@ -2,7 +2,8 @@
 #include "PWM_16bit.h"
 #include "Timer.h"
 
-#define PIN_ENC 3
+#define PIN_ENC_L 2
+#define PIN_ENC_R 3
 #define WAIT 50
 #define LED1 11
 #define LED2 12
@@ -16,9 +17,12 @@ volatile unsigned long enc_t = 0;
 uint16_t rpm = 0;
 uint16_t setpoint;
 uint16_t feedback;
-uint16_t output;
-uint16_t peaks = 0;
-uint16_t counted_peaks = 0;
+uint16_t output_r = 0;
+uint16_t output_l = 0;
+uint16_t peaks_r = 0;
+uint16_t peaks_l = 0;
+uint16_t counted_peaks_r = 0;
+uint16_t counted_peaks_l = 0;
 Timer t;
 float Kp = 0.1, Ki = 0.5, Kd = 0, Hz = 10;
 int output_bits = 16;
@@ -33,8 +37,10 @@ void setup()
   pinMode(PIN_DIRECTION, OUTPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
-  pinMode(PIN_ENC, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(PIN_ENC), EncISR, RISING);
+  pinMode(PIN_ENC_R, INPUT_PULLUP);
+  pinMode(PIN_ENC_L, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENC_R), EncISR_R, RISING);
+  attachInterrupt(digitalPinToInterrupt(PIN_ENC_L), EncISR_L, RISING);
   digitalWrite(PIN_ENABLE, LOW);
   digitalWrite(PIN_DIRECTION, HIGH);
   t.every(500, readenc, 0);
@@ -44,22 +50,41 @@ void setup()
 void loop()
 {
   if (control) {
- 
+ //right PI control
     setpoint = 200;
-    feedback = counted_peaks*4;
-    output = myPID.step(setpoint, feedback);
+    feedback = counted_peaks_r*4;
+    output_r = myPID.step(setpoint, feedback);
 
-    if (output >= 1023) {
-      output = 1023;
+    if (output_r >= 1023) {
+      output_r = 1023;
     }
-    Serial.print(output);
+    
+//Debug output
+    Serial.print(output_r);
     Serial.print(";");
-    Serial.println(counted_peaks);
+    Serial.print(counted_peaks_r);
+    Serial.print(";");
+    
+//left PI control
+
+    feedback = counted_peaks_l*2;
+    output_l = myPID.step(setpoint, feedback);
+
+    if (output_l >= 1023) {
+      output_l = 1023;
+    }
+
+//Debug output
+    Serial.print(output_l);
+    Serial.print(";");
+    Serial.println(counted_peaks_l);
+
+//write to motor
     if (digitalRead(PIN_ENABLE) == HIGH) {
       digitalWrite(LED1, LOW);
       digitalWrite(LED2, LOW);
     } else {
-      writeLED(output);
+      writeLED(output_r, output_l);
     }
     control=false;
   }
@@ -67,20 +92,27 @@ void loop()
 }
 
 
-void writeLED(uint16_t n)
+void writeLED(uint16_t n1, uint16_t n2)
 {
-  analogWrite16(LED1, 1023 - n);
-  analogWrite16(LED2, 1023 - n);
+  analogWrite16(LED1, 1023 - n1);
+  analogWrite16(LED2, 1023 - n2);
 }
 
 
-void EncISR(void)
+void EncISR_L(void)
 {
-  peaks++;
+  peaks_l++;
+}
+
+void EncISR_R(void)
+{
+  peaks_r++;
 }
 
 void readenc(void* context) {
-  counted_peaks = peaks;
+  counted_peaks_r = peaks_r;
+  counted_peaks_l = peaks_l;
   control = true;
-  peaks = 0;
+  peaks_r = 0;
+  peaks_l = 0;
 }
