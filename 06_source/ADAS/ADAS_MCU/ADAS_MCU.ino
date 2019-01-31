1a63df343688fe7f5e55e15dd33d31b974c0ac4d
jmueller <jmueller2@studenten.hs-bremerhaven.de>
2019-01-31 14:54:34 +0100

    Doxygen comment

--- 06_source/ADAS/ADAS_MCU/ADAS_MCU.ino
+++ 06_source/ADAS/ADAS_MCU/ADAS_MCU.ino
@@ -1,19 +1,32 @@
+/**
+* @file ADAS_NavU.ino
+* @author Hannes Bähr, Juliane Müller
+* @date January 29, 2019
+* @brief Main file for the NavU of the ADAS project
+*
+* 
+* \mainpage Description
+* This is the documentation of the Motor Contol Unit (MCU) of the ADAS project.
+*
+*/
+
+/** Basics */
 #include "ADAS_Types.h"
 #include "ADAS_Cfg.h"
 #include "ADAS_Debug.h"
 
-/*Periperals*/
+/** Periperals */
 #include "HAL_DriveUnit.h"
 #include "HAL_Encoder.h"
 #include "HAL_IMU.h"
 #include "HAL_PWM.h"
 #include "HAL_Serial_IF.h"
 
-/*Comms layer*/
+/** Comms layer */
 #include "Comm_Inertial.h"
 #include "Comm_ICC.h"
 
-/*App Layer*/
+/** App Layer */
 #include "App_MotorCtrl.h"
 #include "App_Stateflow.h"
 #include "App_Stateflowtypes.h"
@@ -34,7 +47,6 @@ void setup() {
   //Hw initialization
   Serial.begin(9600);
   DPRINTLN("Hello\n\r");
-
   //inertial_o.Init();
   mCtrl_o.Init();
   iccPort.Init();
@@ -46,6 +58,7 @@ void loop() {
   iccComms_o.Run();
   mCtrl_o.Run();
 }
+
 // UART2 interrupt
 ISR(USART2_RX_vect)
 {
