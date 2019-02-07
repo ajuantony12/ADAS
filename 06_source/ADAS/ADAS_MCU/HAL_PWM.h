e2ddf7240cd677233114d7e181cd8aa781404df4
jmueller <jmueller2@studenten.hs-bremerhaven.de>
2019-02-07 12:19:01 +0100

    Header adaption

--- 06_source/ADAS/ADAS_MCU/HAL_PWM.h
+++ 06_source/ADAS/ADAS_MCU/HAL_PWM.h
@@ -1,20 +1,32 @@
-/*H**********************************************************************
-* FILENAME :        PWM.h
+/**
+* @file HAL_PWM.h
+* @author Christoph Jurczyk
+* @date January 29, 2019
+* @brief Hardware Abstraction Layer (HAL) for the 10-bit PWM outputs
 *
-* DESCRIPTION : PWM signal header file
-*************************************************************************/
-
-#ifndef PWM_16bit_H
-#define PWM_16bit_H
+*/
+​
+#ifndef HAL_PWM_H
+#define HAL_PWM_H
 #include "ADAS_Types.h"
 #include "ADAS_Cfg.h"
-
+​
+//! Hardware Abstraction Layer (HAL) PWM class
 class CPWMUnit{
 public:
-    typedef enum {
-        PWM1=PIN_MTR_L_PWM,
-        PWM2=PIN_MTR_R_PWM,
-      }PWMID_e;
+
+  /**
+  *
+  * \enum  PWMID_e
+  *
+  * \brief  Enum class for PWM pins
+  **/
+  typedef enum {
+		/// Definition of first PWM pin (OC1A)
+    PWM1=PIN_MTR_L_PWM,
+		/// Definition of second PWM pin (OC1B)
+    PWM2=PIN_MTR_R_PWM,
+   }PWMID_e;
     /*
     * Constructor
     */
